5d0092aa2404ccabd8bec46af03a98af4ef2f413
decoder <bhawarparth@gmail.com>
2024-06-28 14:51:49 +0530

    .

--- index.php
+++ index.php
@@ -597,77 +597,7 @@
 
 
       <!-- Footer section start -->
-      <footer class="footer__area">
-        <div class="footer__top-wrapper">
-          <div class="container">
-            <div class="footer__top">
-              <div class="footer__logo">
-                <img src="assets/imgs/logo/logo-2.png" alt="Logo">
-              </div>
-              <div class="footer__widget">
-                <h3 class="footer__title">Who We Are</h3>
-                <ul class="footer__menu">
-                  <li><a href="about-us.html">About Us</a></li>
-                  <li><a href="portfolio.html">Works</a></li>
-                  <li><a href="blog.html">Blog</a></li>
-                  <li><a href="contact.html">Contact Us</a></li>
-                </ul>
-              </div>
-              <div class="footer__widget">
-                <h3 class="footer__title">Services</h3>
-                <ul class="footer__menu">
-                  <li><a href="service-details.html">Graphic Design</a></li>
-                  <li><a href="service-details.html">Product Design</a></li>
-                  <li><a href="service-details.html">Web Development</a></li>
-                  <li><a href="service-details.html">Digital Marketing</a></li>
-                </ul>
-              </div>
-              <div class="footer__newsletter">
-                <h3 class="footer__title">Get In Touch</h3>
-                <div class="newsletter">
-                  <form action="#">
-                    <input type="email" name="email" placeholder="info@yourmail.com">
-                    <button type="submit" class="submit"><i class="fa-solid fa-arrow-right"></i></button>
-                    <label for="note">
-                      <input type="checkbox" name="note" id="note">
-                      by signing up, you agree to receive mail.
-                    </label>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="footer__btm-wrapper">
-          <div class="container">
-            <div class="footer__btm">
-              <div class="footer__contact">
-                <h4 class="title">Mail Us</h4>
-                <a href="mailto:info@yourmail.com">info@yourmail.com</a>
-              </div>
-              <div class="footer__contact">
-                <h4 class="title">Call Us</h4>
-                <a href="tel:1234567899">(00) 123 456 7899</a>
-              </div>
-              <div class="footer__contact">
-                <h4 class="title">Office Address</h4>
-                <a>255 Sheet, New Town, Ukrian</a>
-              </div>
-              <div class="footer__social-wrapper">
-                <ul class="footer__social">
-                  <li><a href="https://www.facebook.com/codexup" target="_blank"><i
-                        class="fa-brands fa-facebook-f"></i></a></li>
-                  <li><a href="https://twitter.com/" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
-                  <li><a href="https://www.linkedin.com/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
-                  </li>
-                  <li><a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <?php include('./assets/include/footer.php') ?>
       <!-- Footer section end -->
 
 
